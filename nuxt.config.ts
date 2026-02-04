49d4f54a81b7e1ec122b211f7e2f62be395c08ac
javier.pena <javierpb6@gmail.com>
2026-02-04 12:38:52 +0100

    Fixed css error that didn't detect the scss file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,23 @@
+import tsconfigPaths from 'vite-tsconfig-paths'
+import { fileURLToPath } from 'url'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: ['@/assets/scss/main.scss'],
+  alias: {
+    '@': fileURLToPath(new URL('.', import.meta.url))
+  },
+
+  vite: {
+    plugins: [tsconfigPaths()],
+    css: {
+      preprocessorOptions: {
+        scss: {
+          quietDeps: true,
+          silenceDeprecations: ['legacy-js-api', 'import'],
+        }
+      }
+    }
+  }
 })
