246bfa062089ffa5d9da3d3ccbe686eecf209dfb
javier.pena <javierpb6@gmail.com>
2026-02-05 14:39:21 +0100

    Added type logic

--- pages/todos/[action]/[type].vue
+++ pages/todos/[action]/[type].vue
@@ -1,10 +1,33 @@
 <template>
-    <TodoForm />
+    <TodoForm :todo="todo" :isEdit="isEdit" />
 </template>
 
 <script setup lang="ts">
 
+const route = useRoute();
 
+const todoStore = useTodoStore();
+const { todos } = storeToRefs(todoStore);
+
+const isEdit = computed(() => route.params.action === 'edit');
+
+const todo = ref({
+    id: 0,
+    title: '',
+    description: '',
+    completed: false,
+    createdAt: new Date().toISOString(),
+})
+
+onMounted(() => {
+    if(isEdit.value){
+        const todoId = Number(route.params.type);
+        const foundTodo = todos.value.find((t) => t.id === todoId);
+        if(foundTodo){
+            todo.value = {...foundTodo};
+        }
+    }
+});
 </script>
 
 <style scoped>
