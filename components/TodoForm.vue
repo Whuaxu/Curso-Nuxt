5a1988a562da40e2199c93655a893081ca72d912
javier.pena <javierpb6@gmail.com>
2026-02-05 14:32:32 +0100

    Added TodoForm logic

--- components/TodoForm.vue
+++ components/TodoForm.vue
@@ -1,28 +1,72 @@
 <template>
-    <form>
+    <form @submit.prevent="handleSubmit()">
         <div>
             <label for="title">Title:</label>
-            <input type="text" id="title" name="title" required/>
+            <input type="text" id="title" name="title" v-model="form.title" required/>
         </div>
 
         <div>
             <label for="description">Description:</label>
-            <input type="text" id="description" name="description" required/>
+            <input type="text" id="description" name="description" v-model="form.description" required/>
         </div>
 
-        <button type="submit" class="btn-success">Add ToDo</button>
+        <button type="submit" class="btn-success">
+            {{ isEdit ? 'Update ToDo' : 'Add ToDo' }}
+        </button>
     </form>
 </template>
 
 <script setup lang="ts">
 
+const router = useRouter();
+
+const todoStore = useTodoStore();
+
+const props = defineProps({
+    todo: {
+
+        type: Object as PropType<{ 
+            id: number,
+            title: string,
+            description: string, 
+            completed: boolean, 
+            createdAt: string
+        }>,
+        required: true
+    },
+    isEdit: {
+        type: Boolean as PropType<boolean>,
+        required: true,
+    },
+
+});
+
+const form = ref({...props.todo});
+
+const handleSubmit = () => {
+    
+    if(props.isEdit){
+        todoStore.updateTodo(form.value);
+    } else {
+        form.value.id = Date.now();
+        todoStore.addTodo(form.value);
+    }
+    router.push(`/todos/${form.value.id}`);
+};
+
+watch(
+    () => props.todo,
+    (newVal) => {
+        form.value = { ...newVal };
+    }
+);
 </script>
 
 <style scoped>
 form{
     background-color: white;
     padding: 10px;
-    margin: 20px 0;
+    margin: 20px;
     border-radius: 5px;
     box-shadow: 0 0 5px rgba(0,0,0,0.5);
 }
